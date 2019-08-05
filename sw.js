8524d038a8d2912f01d21f78c60a3a1cd5c3f811
iamcosmin <iamcosmin@users.noreply.github.com>
2019-08-05 11:17:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,27 @@
-importScripts('/cache-polyfill.js');
+const version = "0.6.16";
+const cacheName = `lybra`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
 
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/',
-       '/index.html',
-     ]);
-   })
- );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
