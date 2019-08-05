ac589c9f479bb667eb415db58ab4488746975438
iamcosmin <iamcosmin@users.noreply.github.com>
2019-08-05 11:12:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,13 @@
-var cacheName = 'lybra-world-page';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/styles.css'
-  '/manifest.json'
-  '/favicon.png'
-  '/sw.js'
-  '/allo/'
-];
+importScripts('/cache-polyfill.js');
+
+
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+     ]);
+   })
+ );
 });
