30cb27cf609fcf942d4a58cd8ad1a5b0aa9aaa71
iamcosmin <iamcosmin@users.noreply.github.com>
2019-08-31 10:42:30 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker is registered', swReg);
 
     swRegistration = swReg;
+    initializeUI()
   })
   .catch(function(error) {
     console.error('Service Worker Error', error);
@@ -14,3 +15,12 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.warn('Push messaging is not supported');
   pushButton.textContent = 'Push Not Supported';
 }
+
+var button = document.getElementById("notifications");
+button.addEventListener('click', function(e) {
+    Notification.requestPermission().then(function(result) {
+        if(result === 'granted') {
+            randomNotification();
+        }
+    });
+});
