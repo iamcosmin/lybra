7b810710dbb4cf67fcf15b98c2ff6e10c0c22a64
iamcosmin <iamcosmin@users.noreply.github.com>
2019-08-31 17:04:05 +0300

    NEW!
    Notification API beta test.

---
+++ scripts/Notifications.js
@@ -0,0 +1,16 @@
+const showLocalNotification = (title, body, swRegistration) => {
+
+    const options = {
+        "body": "Buna ziua!",
+
+    };
+
+    swRegistration.showNotification(title, options);
+}
+const main = async () => {
+    check();
+    const swRegistration = await registerServiceWorker();
+    const permission =  await requestNotificationPermission();
+    showLocalNotification('This is title', 'this is the message', swRegistration);
+}
+main();
