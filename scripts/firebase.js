10c95b3332a1b173934dc47040e01b593c471faa
iamcosmin <iamcosmin@users.noreply.github.com>
2019-08-31 10:27:08 +0300

    Firebase integration+ service worker rework

---
+++ scripts/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCJEFD78-QaB234ryRSJCEPOQPyn6FDU14",
+    authDomain: "lybra-store.firebaseapp.com",
+    databaseURL: "https://lybra-store.firebaseio.com",
+    projectId: "lybra-store",
+    storageBucket: "",
+    messagingSenderId: "597588244397",
+    appId: "1:597588244397:web:5581e4171fcce06b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
