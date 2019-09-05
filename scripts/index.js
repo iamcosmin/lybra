b5588322c63eccee90147b3f7441520a7ea575bb
iamcosmin <iamcosmin@users.noreply.github.com>
2019-09-05 11:30:08 +0300

    Maintanance Break!

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,2 @@
 navigator.serviceWorker.register('/scripts/sw.js');
-
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-});
+alert('Maintanace Break! Momentan site-ul Lybra este sub mentenanta. Incercati mai tarziu.')
