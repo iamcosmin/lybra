ff15459d878ddbbf0ae454f785c2c08d3251d6e1
iamcosmin <iamcosmin@users.noreply.github.com>
2019-09-15 16:25:24 +0300

    Dark mode partial si pregatiri pentru ultimele proiecte.

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,9 @@ navigator.serviceWorker.register('/scripts/sw.js');
 function inapoi() {
    window.history.back();
 }
-
+function activateDark() {
+   localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark'); localStorage.getItem('mode') === 'dark' ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+}
 document.addEventListener('DOMContentLoaded', (event) => {
-   ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
- })
+  ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+})
