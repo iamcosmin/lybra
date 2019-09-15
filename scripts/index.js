ed4a39c13bf03cfde3933e38a54673c761c2445e
iamcosmin <iamcosmin@users.noreply.github.com>
2019-09-15 13:19:23 +0300

    Working...

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,7 @@ navigator.serviceWorker.register('/scripts/sw.js');
 function inapoi() {
    window.history.back();
 }
+
+document.addEventListener('DOMContentLoaded', (event) => {
+   ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark')
+ })
