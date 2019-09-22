d117b672fceb30b7699b8d465ad11cc76bc14b4f
iamcosmin <iamcosmin@users.noreply.github.com>
2019-09-22 17:02:41 +0300

    ....

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,18 @@ function activateDark() {
   localStorage.getItem("mode") === "dark"
     ? document.querySelector("body").classList.add("dark")
     : document.querySelector("body").classList.remove("dark");
-  localStorage.getItem("mode") === "dark"
-    ? document.querySelector(".line").classList.add("dark")
-    : document.querySelector(".line").classList.remove("dark");
 }
 document.addEventListener("DOMContentLoaded", event => {
   (localStorage.getItem("mode") || "dark") === "dark"
     ? document.querySelector("body").classList.add("dark")
     : document.querySelector("body").classList.remove("dark");
-  (localStorage.getItem("mode") || "dark") === "dark"
-    ? document.querySelector(".line").classList.add("dark")
-    : document.querySelector(".line").classList.remove("dark");
 });
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function openNavBar() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
