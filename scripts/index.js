00506aa34342d0bb2002535bdea262573273310f
iamcosmin <iamcosmin@users.noreply.github.com>
2019-09-29 11:21:48 +0300

    New Maintanance Sistem

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,34 @@
+// Maintanance Break Tool
+const maintanance = "none";
+let main = document.getElementById("main");
+let maintatancePack = document.getElementById("maintanance");
+let maintananceHeader = document.querySelector("h1");
+let maintananceParagraph = document.querySelector("p");
+
+if (maintanance == "long") {
+  main.style.display = "none";
+  maintananceHeader.textContent = "Mentenanță în curs!";
+  maintananceParagraph.textContent = "Vom fi înapoi în 1 oră.";
+} else if (maintanance == "short") {
+  main.style.display = "none";
+  maintananceHeader.textContent = "Mentenanță în curs!";
+  maintananceParagraph.textContent =
+    "Vom fi înapoi în mai puțin de 30 de minute.";
+} else if (maintanance == "hotfix") {
+  main.style.display = "none";
+  maintananceHeader.textContent = "Mentenanță în curs!";
+  maintananceParagraph.textContent =
+    "Trebuie să rezolvăm o problemă critică. Va dura ceva.";
+} else if (maintanance == "scheduled") {
+  alert("Site-ul se închide în 5 minute pentru o scurtă mentenanță.");
+} else if (maintanance == "none") {
+  console.log("Nicio mentenanta planificata");
+  maintatancePack.style.display = "none";
+}
+//End of Maintanance Break Tool
+
+//Dark Mode Tool
+
 function activateDark() {
   localStorage.setItem(
     "mode",
@@ -12,7 +43,9 @@ document.addEventListener("DOMContentLoaded", event => {
     ? document.querySelector("body").classList.add("dark")
     : document.querySelector("body").classList.remove("dark");
 });
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+//End of Dark Mode Tool
+
+//Toggle NavBar Tool
 function openNavBar() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -21,3 +54,4 @@ function openNavBar() {
     x.style.display = "block";
   }
 }
+//End of Toggle NavBar Tool
