aa37425e47c17f7e887cfa1bd1a283becff7a14b
iamcosmin <iamcosmin@users.noreply.github.com>
2019-10-19 11:56:32 +0300

    Critical Push

--- scripts/home.js
+++ scripts/home.js
@@ -1,24 +1,24 @@
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
   var profile = googleUser.getBasicProfile();
-  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-  console.log("Full Name: " + profile.getName());
-  console.log("Given Name: " + profile.getGivenName());
-  console.log("Family Name: " + profile.getFamilyName());
-  console.log("Image URL: " + profile.getImageUrl());
-  console.log("Email: " + profile.getEmail());
   const conectatH1 = document.querySelector(".welcome-h1");
   const conectatP = document.querySelector(".welcome-p");
-  conectatH1.textContent = "Bine ai venit," + profile.getGivenName();
-
-  // The ID token you need to pass to your backend:
-  var id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
+  conectatH1.textContent = "Bine ai venit," + " " + profile.getGivenName();
+  conectatP.textContent = "Acum, ai acces la toate serviciile.";
 }
 
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function() {
-    console.log("User signed out.");
+    const conectatH1 = document.querySelector(".welcome-h1");
+    const conectatP = document.querySelector(".welcome-p");
+    conectatH1.textContent = "Ai fost deconectat.";
+    conectatP.textContent = "Pentru a te deconecta complet, reincarca pagina.";
   });
 }
+//   console.log("ID: " + profile.getId());
+//   console.log("Full Name: " + profile.getName());
+//   console.log("Given Name: " + profile.getGivenName());
+//   console.log("Family Name: " + profile.getFamilyName());
+//   console.log("Image URL: " + profile.getImageUrl());
+//   console.log("Email: " + profile.getEmail());
