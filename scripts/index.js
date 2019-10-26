52c8ec53bba9d855a87f9a75a1596024329e7869
iamcosmin <iamcosmin@users.noreply.github.com>
2019-10-26 11:02:36 +0300

    FAST

--- scripts/index.js
+++ scripts/index.js
@@ -22,5 +22,16 @@ function openNavBar() {
   } else {
     x.style.display = "block";
   }
-}
+};
+
+const user = netlifyIdentity.currentUser();
+ 
+// Bind to events
+netlifyIdentity.on('init', user => console.log('init', user));
+netlifyIdentity.on('login', user => console.log('login', user));
+netlifyIdentity.on('logout', () => console.log('Logged out'));
+netlifyIdentity.on('error', err => console.error('Error', err));
+netlifyIdentity.on('open', () => console.log('Widget opened'));
+netlifyIdentity.on('close', () => console.log('Widget closed'));
+
 //End of Toggle NavBar Tool
