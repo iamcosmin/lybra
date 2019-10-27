66b9614e43c2211c627548df4cd5249030fe2f67
iamcosmin <iamcosmin@users.noreply.github.com>
2019-10-27 11:19:55 +0200

    FIXES.

--- scripts/music.js
+++ scripts/music.js
@@ -9,7 +9,7 @@ function openBar() {
     body.style.overflowY = "scroll";
   }
 }
-function activateDark() {
+function activateDark1() {
   localStorage.setItem(
     "mode",
     (localStorage.getItem("mode") || "dark") === "dark" ? "light" : "dark"
@@ -22,4 +22,24 @@ document.addEventListener("DOMContentLoaded", event => {
   (localStorage.getItem("mode") || "dark") === "dark"
     ? document.querySelector("body").classList.add("dark")
     : document.querySelector("body").classList.remove("dark");
-  });
+});
+function activateDark2(){
+  localStorage.setItem(
+    "mode",
+    (localStorage.getItem("mode") || "dark") === "dark" ? "light" : "dark"
+  );
+  localStorage.getItem("mode") === "dark"
+    ? document.querySelector("nav").classList.add("dark")
+    : document.querySelector("nav").classList.remove("dark");
+}
+
+document.addEventListener("DOMContentLoaded", event => {
+  (localStorage.getItem("mode") || "dark") === "dark"
+    ? document.querySelector("nav").classList.add("dark")
+    : document.querySelector("nav").classList.remove("dark");
+});
+function activateDark(){
+  activateDark1();
+  activateDark2();
+}
+
