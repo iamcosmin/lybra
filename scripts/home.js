64fdc634e7c2ed5dab02b994716c52dfbfea63b3
iamcosmin <iamcosmin@users.noreply.github.com>
2019-10-27 19:05:54 +0200

    Fixes
    Acum, meniul de navigare este disponibil pentru prima pagina.

--- scripts/home.js
+++ scripts/home.js
@@ -1,24 +1,11 @@
-function onSignIn(googleUser) {
-  // Useful data for your client-side scripts:
-  var profile = googleUser.getBasicProfile();
-  const conectatH1 = document.querySelector(".welcome-h1");
-  const conectatP = document.querySelector(".welcome-p");
-  conectatH1.textContent = "Bine ai venit," + " " + profile.getGivenName();
-  conectatP.textContent = "Acum, ai acces la toate serviciile.";
+function openBar() {
+  var x = document.getElementById("navigate");
+  var body = document.getElementById("body");
+  if (x.className === "navigatore") {
+    x.className += " responsive";
+    body.style.overflow = "hidden";
+  } else {
+    x.className = "navigatore";
+    body.style.overflowY = "scroll";
+  }
 }
-
-function signOut() {
-  var auth2 = gapi.auth2.getAuthInstance();
-  auth2.signOut().then(function() {
-    const conectatH1 = document.querySelector(".welcome-h1");
-    const conectatP = document.querySelector(".welcome-p");
-    conectatH1.textContent = "Ai fost deconectat.";
-    conectatP.textContent = "Pentru a te deconecta complet, reincarca pagina.";
-  });
-}
-//   console.log("ID: " + profile.getId());
-//   console.log("Full Name: " + profile.getName());
-//   console.log("Given Name: " + profile.getGivenName());
-//   console.log("Family Name: " + profile.getFamilyName());
-//   console.log("Image URL: " + profile.getImageUrl());
-//   console.log("Email: " + profile.getEmail());
